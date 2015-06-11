f586f2827443f437dfece994c420029de453208a
Louis Wilbrink <louis.wilbrink@gmail.com>
2015-06-10 21:24:31 -0600

    getting user data from decorating directive

--- app.js
+++ app.js
@@ -4,25 +4,27 @@ angular.module('myapp').directive('userProfile', function () {
     var directiveDefinitionObject = {
         scope: {},
         restrict: 'E',
+        require: ['queryUser'],
         template: '<div>Hello, {{ name }}</div>',
-        controller: function ($scope, $rootScope) {
-            console.log('userProfile - $id', $scope.$id);
-            $scope.name = 'Louis';
-        },
         link: function (scope, element, attrs) {
-            console.log(attrs.queryUserProfile);
+            attrs.$observe('queryUser', function (newVal) {
+                scope.name = JSON.parse(newVal).name;
+            });
         }
     };
 
     return directiveDefinitionObject;
 });
 
-angular.module('myapp').directive('queryUserProfile', function () {
+angular.module('myapp').directive('queryUser', function () {
     var directiveDefinitionObject = {
         restrict: 'A',
-        controller: function ($scope, UserService) {
-            console.log('queryUserProfile - $id', $scope.$id);
-            $scope.age = UserService;
+        controller: function ($scope, UserService, $timeout) {
+            $scope.user = { name: '---' };
+
+            $timeout(function () {
+                $scope.user = UserService;
+            }, 1000);
         }
     };
 
@@ -30,12 +32,11 @@ angular.module('myapp').directive('queryUserProfile', function () {
 });
 
 angular.module('myapp').service('UserService', function ($rootScope) {
-    console.log('$rootScope Id', $rootScope.$id);
 
     return {
         name: 'Louis',
         age: 15,
         address: '123 Fake Street',
-        email: 'louis@louiswilbrink.com'
+        email: 'louis@fakedomain.com'
     }
 });
